40bd02a76fb3732dc8868dddc31040128417e1d1
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-22 22:46:14 -0500

    Feature: New Project, Double Tap Gesture

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Oscar David Myerston Vega on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ee5490ea03e1b53dcaddfbd32292b3b73ba83d70
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-23 13:59:01 -0500

    Feature: Long Press Gesture

--- Pinch/ContentView.swift
+++ Pinch/ContentView.swift
@@ -25,8 +25,8 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -70,6 +70,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             }
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
 
         }//: NAVIGATION
         .navigationViewStyle(.stack)

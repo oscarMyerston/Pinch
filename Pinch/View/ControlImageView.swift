16825e5932f53a828ecf30ca691139e5a29f75bf
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-23 15:21:41 -0500

    Feature: Controle Interface

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Oscar David Myerston Vega on 23/06/24.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
